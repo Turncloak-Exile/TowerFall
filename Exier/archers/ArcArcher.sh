b27a9529449d4bfc3a89157db3c59375e3533d02
Mark <Mark.Davies95@Outlook.com>
2014-12-07 17:39:05 +0000

    Corrected ANSWERS in QuestArcher.sh Added Options in ArcArcher.sh

---
+++ Exier/archers/ArcArcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 1
+echo -e "\e[0;31mArchives Archer: Loading Archives";
+sleep 2
+echo -e "\e[0;31mQuest Archer: Archives Available:
+
+1)Archers
+2)Art
+3)commandlists
+4)credits";
+
+echo -e "\e[0;31mQuest Archer: Which Quest do you want to launch?";
