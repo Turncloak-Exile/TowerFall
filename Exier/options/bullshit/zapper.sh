951a3b1a47ae6fd63be0bd09260224ae7b6c4f50
Mark <Mark.Davies95@Outlook.com>
2014-12-07 17:24:36 +0000

    Updated Quest Archer ANSWERS and Fixed semi collons on echo commands in Zapper

--- Exier/options/bullshit/zapper.sh
+++ Exier/options/bullshit/zapper.sh
@@ -13,38 +13,34 @@ echo "
 	      /  /   A P P E R
 	     /  /_____
      	/         /
-       /_________/  Written by Ancient Exile
+       /_________/  Written by Ancient Exile";
 
-"
 echo -e "\e[1;33mJarvisZ: Current processes"
-sleep 0.5
-ps
-sleep 2
+	sleep 0.5
+	ps
+	sleep 2
 
-
-echo -e "Enter the PID to kill or type 10 to go close JarvisZ "
+echo -e "Enter the PID to kill or type 10 to go close JarvisZ";
 sleep 2
 
 read ANSWER
 
-
 if [ "ANSWER" == "$ANSWER_EXIT" ];
 	then
 	sleep 1
-	echo -e "\e[1;33mJarvisZ: Now Closing Jarvisz"
+	echo -e "\e[1;33mJarvisZ: Now Closing Jarvisz";
 	sleep 0.5
-	echo -e "\e[1;33mJarvisZ: Returning to Main menu"
+	echo -e "\e[1;33mJarvisZ: Returning to Main menu";
 
 
 else
-	then
 	sleep 0.5
-	echo Closing specified process
+	echo -e "\e[1;34mJarvis: Please type in the desired process to kill";
 	sleep 2
 	kill $ANSWER
-	echo -e "\e[1;34mJarvis: Desired process has terminated"
+	echo -e "\e[1;34mJarvis: Desired process has terminated";
 	sleep 1
-	echo -e "\e[1;34mJarvis: Returning to Main menu"
+	echo -e "\e[1;34mJarvis: Returning to Main menu";
 fi
 done
 
