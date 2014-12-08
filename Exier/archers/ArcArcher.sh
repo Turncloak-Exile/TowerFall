3420cf62ae1aa7eb65a8786afba208f70d817a55
Mark Davies <Mark.Davies95@Outlook.com>
2014-12-08 10:26:15 +0000

    Update ArcArcher.sh
    Added answers to commands,
    Added until command to loop archives options until exier menu is pressed
    Fixed quotes
    Checked chain files

--- Exier/archers/ArcArcher.sh
+++ Exier/archers/ArcArcher.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
 
+ANSWER_ARCHERS="1"
+ANSWER_ART="2"
+ANSWER_USEFUL="3"
+ANSWER_CREDITS="4"
+ANSWER_EXIERMENU="5"
+
+until [ "$ANSWER" == "$ANSWER_EXIERMENU" ];
+do    
+
 sleep 1
-echo -e "\e[0;31mArchives Archer: Loading Archives";
+echo -e "\e[0;31mArchives Archer: Loading libary";
 sleep 2
-echo -e "\e[0;31mQuest Archer: Archives Available:
+echo -e "\e[0;31mArchives Archer: Archives Available:
 
 1)Archers
 2)Art
-3)commandlists
-4)credits";
+3)Useful documents
+4)Credits
+5)Back to Exier";
 
-echo -e "\e[0;31mQuest Archer: Which Quest do you want to launch?";
+echo -e "\e[0;31mArchives Archer: Which Archive would you like to visit?";
+
+if [ "$ANSWER" == "$ANSWER_ARCHERS" ];
+  then
+  echo -e "\e[0;31mArchives Archer: Searching Archives for Archers";
+  sleep 1
+  exier/archives/Archers/archersarchive.sh
+  
+elif [ "$ANSWER" == "$ANSWER_ART" ];
+  then
+  echo -e "\e[0;31mArchives Archer: Searching Archives for Art";
+  sleep 1
+  exier/archives/art/showart.sh
+
+elif [ "$ANSWER" == "$ANSWER_USEFUL" ];
+  then
+  echo -e "\e[0;31mArchives Archer: Searching Archives for Useful";
+  sleep 1
+  exier/archives/useful/showuseful.sh
+
+elif [ "$ANSWER" == "$ANSWER_CREDITS" ];
+  then
+  echo -e "\e[0;31mArchives Archer: Searching Archives for Credits";
+  sleep 1
+  exier/options/credits/creditsJ.sh
+  
+elif [ "$ANSWER" == "$ANSWER_EXIERMENU" ];
+  then
+  echo -e "\e[0;31mArchives Archer: Returning to Exier Menu";
+
+fi
+done
