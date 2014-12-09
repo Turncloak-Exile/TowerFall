04b43565dda05577e913420ce4bf134a4e0f6a81
Mark Davies <Mark.Davies95@Outlook.com>
2014-12-09 14:42:06 +0000

    Create password.sh
    created template for password.sh also added answers to be filled in accordingly and added until command if password is entered in wrong.

---
+++ Exier/password.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_ =" "
+ANSWER_BACK="back"
+
+until [ "$ANSWER" == "$ANSWER_BACK" ];
+do
+
+echo -e "\e[1;33mNOTICE: Do put spaces in between numbers! type back to return to Settings"
+sleep 1
+echo -e "\e[1;32mPlease Enter password for desired Menu"
+
+read ANSWER
+
+if ["$ANSWER" == "$ANSWER_ " ];
+  then
+  
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+elif ["$ANSWER" == "$ANSWER_ " ];
+  then
+
+else
+  echo -e "\e[1;33mKing: The Password you entered is incorrect. Please try again."
