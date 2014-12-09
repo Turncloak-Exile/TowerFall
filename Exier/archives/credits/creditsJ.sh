3b03d42ca5c94e0248000960e639862d1bd58df8
Mark Davies <Mark.Davies95@Outlook.com>
2014-12-09 14:05:20 +0000

    Update creditsJ.sh
    realigned boxes and inserted towerfall logo at the top, I also took out the Jarvis logo as instead we will have have credit options

--- Exier/archives/credits/creditsJ.sh
+++ Exier/archives/credits/creditsJ.sh
@@ -6,48 +6,52 @@ ANSWER_LAUNCH="3"
 ANSWER_EXIT="4"
 echo -e "\e[0;33m
 
-
+ ____ _____  ___                                       __________
+ |___|_____||___|                                     _|__|_  |__|
+ |__ _|__|___|__|                                    |_ _|__|_|__|
+ |__|__|_|______|      ___      ___   _____   _____    |___|___     ___    ___     ___
+      |____|   ____   |___|    |___| |_|___|  |_ __\   |_|_ ___|   /___\   | |     | |
+      |__|_|  /_   \  |  |\    /|  | |_|___   | |>  |  |___|      /  _ _\  |_|     |_|
+      |____| /  /\__\  \_|_\/\/_|_/  |_ ___|  |____/   |___|     /__/_\  \ | |     | |
+      |  | | \  \/  /   \________/   |_|___   |_|\_\   |_|_|     |___|___| |_|___  |_|___
+      |__|_|  \____/     \__/\__/    |_|___|  |_| \_|  ||_/	     |__/ \__| |_____| |_____|	
+      |    |                                           | /        
+      |____|                                           |/
+      |_ _/                                         _
+      |_|/      -Written By Exier-                 | |      __   ___   _  _   _  _
+      |_/         Ancient Exile                    | |___  |  | | \ | | || | | \/ |
+      |/        Turncloak Solider                  |_____| |__| |_\_| |____| |_/\_|
 "
 echo -e "\e[0;32m 
-       CCCCCC  RRRRR   EEEEE  DDDD    II  TTTTTT SSSSSSS
-      CC    CC RR  RR  EE     DD  D   II  TTTTTT SS    
-      CC       RR  RR  EEEEE  DD   D  II    TT   SSSSSSS
-      CC       RRRR    EE     DD   D  II    TT        SS
-      CC    CC RR  R   EE     DD  D   II    TT   SSSSSSS 
-       CCCCCC  RR   R  EEEEE  DDDD    II    TT   SSSSSSS          "     
+                     CCCCCC  RRRRR   EEEEE  DDDD    II  TTTTTT SSSSSSS
+                    C     CC RR  RR  EE     DD  D   II  TTTTTT SS    
+                    CC       RR  RR  EEEEE  DD   D  II    TT   SSSSSSS
+                    CC       RRRR    EE     DD   D  II    TT        SS
+                    CC    CC RR  R   EE     DD  D   II    TT   SSSSSSS 
+                     CCCCCC  RR   R  EEEEE  DDDD    II    TT   SSSSSSS";     
 echo -e "\e[0;33m
 
  					          
-       -TURNCLOAK SOLIDER-               -ANCIENT EXILE-"
+                     -TURNCLOAK SOLIDER-               -ANCIENT EXILE-"
 
 until [ "$ANSWER" == "$ANSWER_EXIT" ];
 do
-
-echo -e "\e[1;32m       
-
-    JJJJJJJJ   AA     RRRRR   VV    VV  II  SSSSSSSS
-       JJ     AAAA    RR  RR   V    V   II  SS      
-       JJ     A  A    RR  RR   VV  VV   II  SSSSSSSS
-       JJ    AAAAAA   RRRR      V  V    II        SS
-   JJ  JJ   AA   AA   RR  R     VVVV    II  SSSSSSSS
-    JJJJ   AAA   AAA  RR   R     VV     II  SSSSSSSS";
-
 sleep 1
-echo -e "\e[0;32mJarvisS: Manual Options:
-    _______________________________________ 
-   |                                       |
-   |                                       |
-   |          2)Jarvis Extensions          |
-   |                                       |
-   |           1)Jarvis Manual             |
-   |                                       |
-   |            3)Exit Jarvis              |
-   |                                       |
-   |           4)Launch Jarvis             |
-   |                                       |
-   |        5) Back to exier Menu          |
-   |                                       |
-   |_______________________________________|";
+echo -e "\e[0;32mJarvisS: Credits Options:
+                         _______________________________________ 
+                        |                                       |
+                        |                                       |
+                        |          2)Jarvis Extensions          |
+                        |                                       |
+                        |           1)Jarvis Manual             |
+                        |                                       |
+                        |            3)Exit Jarvis              |
+                        |                                       |
+                        |           4)Launch Jarvis             |
+                        |                                       |
+                        |        5) Back to exier Menu          |
+                        |                                       |
+                        |_______________________________________|";
 
 read ANSWER 
 
