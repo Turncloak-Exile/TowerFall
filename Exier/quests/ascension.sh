a2856835f52bb54ec3b77ea519b6b1ed9e0c6ad5
Mark <Mark.Davies95@Outlook.com>
2014-12-09 01:41:13 +0000

    added graphics in ascension quest as concept idea

--- Exier/quests/ascension.sh
+++ Exier/quests/ascension.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-echo Ascension Quest is currently in devlopment
-sleep 1
-echo Returning to previous menu
-sleep 1
+ANSWER_NORMAL="1"
+ANSWER=HARDCORE="2"
+ANSWER_MAINMENU="3"
+
+echo Ascension Quest is currently in devlopment:
+echo Loading
+echo -e "\e[0;32mPlease select difficulty;
+
+
+    1)NORMAL         2)HARCORE
+ ______________   ______________
+|   ________   | |   ________   |
+|  /\\\\////\  | |  /########\  |
+| /   \\//   \ | | /   \##/   \ |
+| |___|\/|___| | | |___|##____| |
+|  \\\\\/////  | |  \########/  |
+|   \|    |/   | |   \|    |/   |
+|______________| |______________|";
+
+read ANSWER
+
+if [ "$ANSWER" == "$ANSWER_NORMAL" ]; 
+	then
+	echo Normal mode activated
+	sleep 2
+
+elif [ "$ANSWER" == "$ANSWER_HARDCORE" ]; 
+	then
+	echo "Hadcore mode activaed!"
+	sleep 2
+
+elif [ "$ANSWER" == "$ANSWER_MAINMENU" ]; 
+	then
+	echo main menu
+	sleep 2
+fi
+
+echo end of program;
+
+
+
