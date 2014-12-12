7e119ee9eab06e4271c4cc2a8bb3f2993b032227
Mark <Mark.Davies95@Outlook.com>
2014-12-12 17:35:10 +0000

    Got rid of creating virtual machine from Vs archer

--- Exier/archers/VsArcher.sh
+++ Exier/archers/VsArcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ANSWER_CREATVM="2"
+
 ANSWER_EXIERMENU="5"
 
 until [ "$ANSWER" == "$ANSWER_EXIERMENU" ];
@@ -12,7 +12,7 @@ echo -e "\e[1;34mVs Archer: Available Options:
     _______________________________________ 
    |                                       |
    |                                       |
-   |   2)How to create a virtual machine   |
+   |         VS NOT YET AVAILABLE          |
    |                                       |
    |        5) Back to exier Menu          |
    |                                       |
@@ -20,12 +20,7 @@ echo -e "\e[1;34mVs Archer: Available Options:
 
 read ANSWER
 
-if [ "$ANSWER" == "$ANSWER_CREATEVM" ];
-	then
-	echo -e "\e[1;34mVs Archer: Loading Create Virtual Machine through Towerfall"
-	exier/vs/createvm.sh
-
-elif [ "$ANSWER" == "$ANSWER_EXIERMENU" ];
+if [ "$ANSWER" == "$ANSWER_EXIERMENU" ];
 	then
 	echo -e "\e[1;34mVs Archer: Returning to Exier Menu"
 else
